282c2b9097e1e7da20b67ba3991057dd453b10b6
skarni1805 <52778826+skarni1805@users.noreply.github.com>
2025-11-30 14:17:31 -0600

    Update recipient name in mail handler

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -36,7 +36,7 @@ export default async function handler(
             To: [
               {
                 Email: process.env.MAIL_TO as string,
-                Name: "Vinod Darapu",
+                Name: "Swati Karni",
               },
             ],
             Subject: `${name.toUpperCase()} sent you a message from Portfolio`,
@@ -67,3 +67,4 @@ export default async function handler(
     }
   }
 }
+
