b99c2a27f60735802dbcf574ce73f23c7f6cc991
skarni1805 <52778826+skarni1805@users.noreply.github.com>
2025-12-01 14:18:26 -0600

    Update Head component for new user information

--- app/head.tsx
+++ app/head.tsx
@@ -4,48 +4,49 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
-        content="I am Vinod Darapu, a Skilled Cloud and DevOps Engineer with 10+ years of IT experience, in supporting, automating, and optimizing mission critical Applications in AWS and Azure clouds, leveraging configuration management, CI/CD, and DevOps processes."
+        content="I am Swati Karni, a Skilled Cloud and DevOps Engineer with 12+ years of IT experience, in supporting, automating, and optimizing mission critical Applications in AWS and Azure clouds, leveraging configuration management, CI/CD, and DevOps processes."
       />
       <meta
         name="keywords"
-        content="Vinod Darapu, portfolio, vinod darapu, nextjs portfolio, portfolio design, portfolio website, personal portfolio, freelancer, angular, reactjs"
+        content="Swati Karni, portfolio, Swati Karni, nextjs portfolio, portfolio design, portfolio website, personal portfolio, freelancer, angular, reactjs"
       />
       <meta
         property="og:site_name"
-        content="Vinod Darapu's Portfolio"
+        content="Swati Karni's Portfolio"
       />
       <meta
         property="og:title"
-        content="Vinod Darapu - Devops Engineer"
+        content="Swati Karni - Principal Cloud Engineer"
       />
       <meta
         property="og:description"
-        content="I am Vinod Darapu, a Devops Engineer based in Bangalore, India. I'm a 2014 graduate from Sir Vishveswaraiah Institute of Science and Technology (aka Viswam College), Angallu, Andhra Pradesh. I'm passionate about creating beautiful, functional and user-friendly websites and applications, and I'm constantly pushing myself to learn and grow as a developer."
+        content="I am Swati Karni, a Devops Engineer based in Bangalore, India. I'm a 2014 graduate from Sir Vishveswaraiah Institute of Science and Technology (aka Viswam College), Angallu, Andhra Pradesh. I'm passionate about creating beautiful, functional and user-friendly websites and applications, and I'm constantly pushing myself to learn and grow as a developer."
       />
-      <meta property="og:url" content="https://vd-portfolio-hazel.vercel.app" />
+      <meta property="og:url" content="https://sk-portfolio-hazel.vercel.app" />
       <meta
         property="og:image"
-        content="https://vd-portfolio-hazel.vercel.app/portfolio-fork.png"
+        content="https://sk-portfolio-hazel.vercel.app/portfolio-fork.png"
       />
-      <link rel="canonical" href="https://vd-portfolio-hazel.vercel.app" />
-      <meta name="author" content="Vinod Darapu" />
+      <link rel="canonical" href="https://sk-portfolio-hazel.vercel.app" />
+      <meta name="author" content="Swati Karni" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="summary" />
       <meta
         name="twitter:site"
-        content="https://vd-portfolio-hazel.vercel.app"
+        content="https://sk-portfolio-hazel.vercel.app"
       />
       <meta
         name="application-name"
-        content="Portfolio | Vinod Darapu- Devops Engineer"
+        content="Portfolio | Swati Karni- Principal Cloud Engineer"
       />
       <meta
         name="apple-mobile-web-app-title"
-        content="Portfolio | Vinod Darapu- Devops Engineer"
+        content="Portfolio | Swati Karni- Principal Cloud Engineer"
       />
       <link rel="icon" href="/favicon.ico" />
-      <title>Portfolio | Vinod Darapu- Devops Engineer</title>
+      <title>Portfolio | Swati Karni- Principal Cloud Engineer</title>
     </>
   );
 }
+
 
